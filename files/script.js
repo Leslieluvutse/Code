c9bc1d9ebddb45d2e334c73cb644a2b01a71cd64
Leslieluvutse <flexingleslie@gmail.com>
2024-06-18 17:13:14 +0300

    uncommented everything to let the project be on its good form that is the css

--- files/script.js
+++ files/script.js
@@ -9,12 +9,12 @@ var typed = new Typed(".text", {
           });
           
           
-          // const toTop = document.querySelector(".top");
-          // window.addEventListener("scroll",() =>{
-          //     if (window.pageYOffset > 100){
-          //         toTop.classList.add("active");
-          //     }
-          //     else{
-          //         toTop.classList.remove("active");
-          //     }
-          // })
+          const toTop = document.querySelector(".top");
+          window.addEventListener("scroll",() =>{
+              if (window.pageYOffset > 100){
+                  toTop.classList.add("active");
+              }
+              else{
+                  toTop.classList.remove("active");
+              }
+          })
